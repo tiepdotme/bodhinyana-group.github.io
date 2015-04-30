f792ce1ef2fd4c9bbe23916292ba840106b4c285
Helen Calderon <helencalderon@gmail.com>
2015-04-30 12:25:55 +0100

    Whitespace

--- gruntfile.js
+++ gruntfile.js
@@ -2,34 +2,31 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
+
+
+
+
+
+
+
         imagemin: {
             dynamic: {
                 files: [{
                     expand: true,
-                    cwd: '_build/assets/',
+                    cwd: 'dev/',
                     src: ['**/*.{png,jpg,gif}'],
-                    dest: '/assets/'
+                    dest: 'assets/'
                 }]
             }
         }
-
-        autoprefixer: {
-            options: {
-                broswers: ['>1% in UK'];
-            },
-            your_target: {
-      // Target-specific file lists and/or options go here.
-            },
-        },
-
-
     });
 
     // 3. Where we tell Grunt we plan to use this plug-in.
      grunt.loadNpmTasks('grunt-contrib-imagemin');
-     grunt.loadNpmTasks('grunt-contrib-watch grunt-autoprefixer');
+     grunt.loadNpmTasks('grunt-contrib-watch');
+     grunt.loadNpmTasks('grunt-autoprefixer');
 
     // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-    grunt.registerTask('default', ['imagemin', 'auto-prefixer']);
+    grunt.registerTask('images', ['imagemin']);
 
 };
