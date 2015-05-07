d1ce21b2ea3291a9e2de0ba014449e85608c1be0
Helen Calderon <helencalderon@gmail.com>
2015-05-07 10:20:00 +0100

    Added JS tasks

--- gruntfile.js
+++ gruntfile.js
@@ -2,31 +2,64 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
-
-
-
-
-
-
-
         imagemin: {
             dynamic: {
                 files: [{
                     expand: true,
-                    cwd: 'dev/',
+                    cwd: 'dev/assets/',
                     src: ['**/*.{png,jpg,gif}'],
                     dest: 'assets/'
                 }]
             }
+        },
+
+
+
+        clean: {
+            images: ['assets'],
+            scripts: ['scripts/compiled.js', 'scripts/dev-compiled.js']
+        },
+
+
+
+        concat:{
+
+                options: {
+                    //banner: "---\n---\n", uncomment if Liquid ever needed
+                    separator: '\n',
+                    stripBanners: true,
+                },
+                dist: {
+                        src: ['scripts/**/*.js'],
+                        dest: 'scripts/dev-compiled.js',
+                },
+
+        },
+
+
+
+        uglify:{
+            my_target:{
+                files: {
+                    'scripts/compiled.js': ['scripts/dev-compiled.js']
+                }
+            }
         }
+
+
     });
 
     // 3. Where we tell Grunt we plan to use this plug-in.
-     grunt.loadNpmTasks('grunt-contrib-imagemin');
-     grunt.loadNpmTasks('grunt-contrib-watch');
-     grunt.loadNpmTasks('grunt-autoprefixer');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-svgmin');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+
 
     // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-    grunt.registerTask('images', ['imagemin']);
-
+    grunt.registerTask('images', ['clean:images', 'imagemin']);
+    grunt.registerTask('javascript', ['clean:scripts', 'concat', 'jshint', 'uglify']);
+    grunt.registerTask('jsdev', ['clean:scripts', 'concat', 'jshint']);
 };
