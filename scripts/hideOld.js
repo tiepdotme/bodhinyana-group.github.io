893ce10fd75617c385d0fd0f0d51e76ae149b091
Helen Calderon <helencalderon@gmail.com>
2017-12-10 16:59:26 +0000

    Take meeting off homepage after meeting, not before

--- scripts/hideOld.js
+++ scripts/hideOld.js
@@ -1,6 +1,8 @@
 	function hideOld() {
 		if (!document.getElementsByTagName) return false;
 	    var today = new Date();
+        today = today.setHours(today.getHours()-4);
+        today = new Date(today);
 	    var posts = document.getElementsByTagName("li");
 	    for (var i = 0; i < posts.length; i++)
 	        if (posts[i].id) {
